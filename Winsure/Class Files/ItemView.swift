5d163c8e9387dc9d58886d2752fe327c0357fbdc
Ryan Soanes <ryan@lionstone.app>
2019-07-10 15:17:55 +0100

    minor ui changes

---
+++ Winsure/Class Files/ItemView.swift
@@ -0,0 +1,17 @@
+//
+//  ItemView.swift
+//  Winsure
+//
+//  Created by Ryan Soanes on 10/07/2019.
+//  Copyright © 2019 LionStone. All rights reserved.
+//
+
+import UIKit
+
+class ItemView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
