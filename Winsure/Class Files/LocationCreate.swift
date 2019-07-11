09273c7340cf81f8844376e1287e12579bc43592
Ryan Soanes <ryan@lionstone.app>
2019-07-11 17:22:47 +0100

    Can now take photos with appropriate permission message, photo previews. Data is stored in CoreData as an object (photo, string) and both displayed on location table view.

--- Winsure/Class Files/LocationCreate.swift
+++ Winsure/Class Files/LocationCreate.swift
@@ -8,16 +8,74 @@
 
 import UIKit
 import TextFieldEffects
+import CoreData
 
-class LocationCreate: UIViewController {
+class LocationCreate: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var locationImagePreview: UIImageView!
-    @IBOutlet var takePhotoButton: UIButton!
     @IBOutlet var locationCreateTextField: IsaoTextField!
-    @IBOutlet var locationCreateButton: UIButton!
+    var imageNSData: NSData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+        roundedImage()
     }
+    
+    @IBAction func takePhotoButton(_ sender: UIButton) {
+        let camera = UIImagePickerController()
+        camera.sourceType = .camera
+        camera.allowsEditing = true
+        camera.delegate = self
+        present(camera, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        locationImagePreview.image = image
+        guard let imageData = image.jpegData(compressionQuality: 0.5) else {
+            // handle failed conversion
+            print("jpg error")
+            return
+        }
+        imageNSData = imageData as NSData
+        //print(imageNSData!)
+        
+    }
+    
+    @IBAction func locationCreateButton(_ sender: UIButton) {
+        saveData()
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    func roundedImage() {
+        self.locationImagePreview.layer.cornerRadius = 75
+        self.locationImagePreview.clipsToBounds = true
+    }
+    
+    func saveData() {
+        //refer to AppDelegate core data container
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        //create context for container
+        let managedContext = appDelegate.persistentContainer.viewContext
+        //create an entity for new records
+        let locationEntity = NSEntityDescription.entity(forEntityName: "LocationObject", in: managedContext)!
+        let location = NSManagedObject(entity: locationEntity, insertInto: managedContext)
+        location.setValue(locationCreateTextField.text, forKey: "locationName")
+        location.setValue(imageNSData, forKey: "locationPicture")
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
     
 }
--- Winsure/Class Files/Locations.swift
+++ Winsure/Class Files/Locations.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-class Locations: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class Locations: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet var exportButton: UIBarButtonItem!
     @IBOutlet var locationTable: UITableView!
@@ -17,6 +18,7 @@ class Locations: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let cellColour = [UIColor.red, UIColor.blue, UIColor.green]
     let cellImage = ["blueCellGradient", "greenCellGradient", "orangeCellGradient", "purpleCellGradient"]
+    var locationObjectsArray = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,22 +27,46 @@ class Locations: UIViewController, UITableViewDelegate, UITableViewDataSource {
         locationTable.dataSource = self
         self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "LocationObject")
+        locationObjectsArray.removeAll()
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "locationName") as! String)
+                locationObjectsArray = locationObjectsArray + [data]
+            }
+            locationTable.reloadData()
+        } catch {
+            print("failed homie")
+        }
+    }
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 11
+        return locationObjectsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationXib", for: indexPath) as! LocationXib
         cell.backgroundImage.image = UIImage(named: cellImage[indexPath.row % 4])
         cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        cell.titleLabel.text = locationObjectsArray[indexPath.row].value(forKey: "locationName") as? String
+        let imagetest = locationObjectsArray[indexPath.row].value(forKey: "locationPicture")
+        cell.locationImage.image = UIImage.init(data: imagetest as! Data, scale: 1)
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         self.performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.locationTable.endUpdates()
     }
     
 }
